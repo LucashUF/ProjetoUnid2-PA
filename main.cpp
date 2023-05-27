b8200a9cf12303a1a44592d8f46716e34171058c
matheusdm01 <130093196+matheusdm01@users.noreply.github.com>
2023-05-27 15:37:40 -0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+#include "sculptor.h"
+
+int main() {
+    int size = 150; //lenght of sandbox
+    int halfR = (size / 2) - 1;
+    int halfL = (size / 2);
+      
+  
+  
+  
+  
+  return 0;
+}
